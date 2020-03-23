22c564a324228418b86224812d7f0015f9092b03
dmtarua <44263423+dmtarua@users.noreply.github.com>
2020-03-23 21:26:38 +0000

    selectable maps

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var trafficHour = [86,114,106,106,107,111,133,221,783,2478,86,114,106,106,107,11
 var traffic5Minutes = [282,350,411,502,635,809,947,1402,3700,5267,86,114];
 
 function populateCharts(arg1){
+  document.getElementById("Chart").innerHTML = "";
   var id1 = document.getElementById(arg1);
   if(id1.value == "d1"){
     var chart = document.getElementById("Chart");
@@ -29,7 +30,7 @@ function populateCharts(arg1){
       }
     });
   }
-  if(id1.value == "d2"){
+  else if(id1.value == "d2"){
     var chart = document.getElementById("Chart");
     var myChart = new Chart(chart, {
       type: 'bar',
@@ -53,4 +54,44 @@ function populateCharts(arg1){
   }
 }
 
-//https://tobiasahlin.com/blog/chartjs-charts-to-get-you-started/
+function getMap(arg1){
+  document.getElementById("map").innerHTML = "";
+  var id1 = document.getElementById(arg1);
+  if(id1.value == "aveiro"){
+    // ------------------------------------ MAP --------------------------------------
+    var map = new ol.Map({
+      target: 'map',
+      layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([-8.75278, 40.61771]),
+        zoom: 13})
+    });
+
+    // ------------------------------------ MARKER -----------------------------------
+    var markers = new OpenLayers.Layer.Markers( "Markers" );
+    map.addLayer(markers);
+    
+    markers.addMarker(new OpenLayers.Marker([-8.75278, 40.61771]));
+    
+    map.setCenter (lonLat, zoom);
+  }
+
+  else if(id1.value == "porto"){
+    var map = new ol.Map({
+      target: 'map',
+      layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([-8.61099, 41.14961]),
+        zoom: 13})
+    });
+  }
+}
+
+// Graph.js -> https://tobiasahlin.com/blog/chartjs-charts-to-get-you-started/
+// OpenLayers -> https://openlayers.org/
