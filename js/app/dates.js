670e1b4a56f8dff6349542d779b7456949ffca61
dmtarua <44263423+dmtarua@users.noreply.github.com>
2020-04-11 20:17:42 +0100

    bugfix

--- js/app/dates.js
+++ js/app/dates.js
@@ -9,4 +9,11 @@ function updateDate(arg1){
   date.setDate(date.getDate() - daysPrior);
 
   document.getElementById("homolday").innerHTML="<div class='onscreen'> <span class='fa-stack onscreen'><span class='fa fa-circle-o fa-stack-2x'></span><strong class='fa-stack-1x fa fa-calendar'></strong></span></i> Homologous Date: " +date.toDateString() +"</div>"; 
+
+  var dashValue = document.getElementById("dashinput");
+  var hourValue = document.getElementById("dashhour");
+  if(dashValue.value != "dnull" && dashValue.value != "d2")
+  	populateCharts("dashinput");
+  else if(hourValue.value != "hnull" && dashValue.value == "d2")
+  	updateHourChart("dashhour");
 }
